b944d90c49a8d8eea372aa75d28057c0bb4fbb73
felipeluis223 <lf9072962@gmail.com>
2024-10-11 13:07:39 -0300

    feat: refatorando o código

--- main.py
+++ main.py
@@ -1,84 +1,53 @@
-# Bot para gerar medidas - Esquadria
 import pandas as pd
 
-def porta_giro_suprema(largura, altura, quantidade):
-    marco_largura_desconto = largura - (28+26) #SU004
-    marco_altura_desconto = altura - 15 #SU007
-    
-    batente_largura_desconto = marco_largura_desconto #SU279
-    batente_altura_desconto = altura - (15+17) #SU279
-    
-    folha_largura_desconto = largura - (28+26+64) #SU241
-    folha_altura_desconto = altura - (15+17+42) #SU241
+def item(perfil, medida, corte, quantidade, descricao, altura_largura):
+    return {
+        'Perfil': perfil,
+        'Medida': medida,
+        'Corte': corte,
+        'Quantidade': int(quantidade),
+        'Descricao': descricao,
+        'L/H': altura_largura
+    }
+ # Lembrando que está contando o CM:
+class Suprema:
+    def porta_giro(self, width, height, amount):
+        width_marco = width - (28+26) #SU004
+        height_marco = height - 15 #SU007
 
-    veneziana_ventilada_largura = largura - (28+26+64+100) #US-294
+        width_batente = width_marco #SU279
+        height_batente = height - (15+17) #SU279
 
-    return [
-        {
-            'Perfil': 'SU004',
-            'Medida': marco_largura_desconto,
-            'Corte': '90/90',
-            'Quantidade': quantidade,
-            'Descricao': 'Marco',
-            'L/H': 'L'
-        },
-        {
-            'Perfil': 'SU007',
-            'Medida': marco_altura_desconto,
-            'Corte': '90/90',
-            'Quantidade': quantidade * 2,
-            'Descricao': 'Marco',
-            'L/H': 'H'
-        },
-        {
-            'Perfil': 'SU279',
-            'Medida': batente_largura_desconto,
-            'Corte': '45/45',
-            'Quantidade': quantidade,
-            'Descricao': 'Batente',
-            'L/H': 'L'
-        },
-        {
-            'Perfil': 'SU279',
-            'Medida': batente_largura_desconto,
-            'Corte': '45/90',
-            'Quantidade': quantidade,
-            'Descricao': 'Batente',
-            'L/H': 'H'
-        },
-        {
-            'Perfil': 'SU279',
-            'Medida': batente_largura_desconto,
-            'Corte': '90/45',
-            'Quantidade': quantidade,
-            'Descricao': 'Batente',
-            'L/H': 'H'
-        },
-        {
-            'Perfil': 'SU241',
-            'Medida': folha_largura_desconto,
-            'Corte': '45/45',
-            'Quantidade': quantidade,
-            'Descricao': 'Folha',
-            'L/H': 'L'
-        },
-        {
-            'Perfil': 'SU241',
-            'Medida': folha_altura_desconto,
-            'Corte': '45/45',
-            'Quantidade': quantidade,
-            'Descricao': 'Folha',
-            'L/H': 'H'
-        },
-        {
-            'Perfil': 'US-294',
-            'Medida': veneziana_ventilada_largura,
-            'Corte': '90/90',
-            'Quantidade': ((folha_altura_desconto-100)/60)*quantidade,
-            'Descricao': 'Venezianda Ventilada',
-            'L/H': 'L'
-        }
-    ]
+        width_folha = width - (28+26+64) #SU241
+        height_folha = height - (15+17+42) #SU241
 
-data = porta_giro_suprema(700, 2120, 1)
-df = pd.DataFrame(data)
+        width_veneziana_ventilada = width -(28+26+64+100) #US-294
+        amount_veneziana = int(((height_folha-100)/60)*amount)
+
+        return [
+            item("SU004", width_marco, "90/90", amount, "Marco", "L"),
+            item("SU007", height_marco, "90/90", (amount * 2), "Marco", "H"),
+
+            item("SU279", width_batente, "45/45", amount, "Batente", "L"),
+            item("SU279", height_batente, "45/90", amount, "Batente", "H"),
+            item("SU279", height_batente, "90/45", amount, "Batente", "H"),
+
+            item("SU241", width_folha, "45/45", amount, "Folha", "L"),
+            item("SU241", height_folha, "45/45", amount, "Folha", "H"),
+
+            item("US-294", width_veneziana_ventilada, "90/90",amount_veneziana, "Folha", "L"),
+        ]
+
+esquadria = Suprema()
+
+print("\nPorta de Giro - Linha Suprema\n")
+
+width = int(input("Digite a largura(mm): "))
+height =  int(input("Digite a altura(mm): "))
+amount = int(input("Digite a quantidade de peças: "))
+
+df = pd.DataFrame(
+    esquadria.porta_giro(width, height, amount), 
+    columns=['Perfil', 'L/H', 'Quantidade', 'Descricao' , 'Corte', 'Medida']
+)
+print(df)
